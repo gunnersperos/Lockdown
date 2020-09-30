48c4161551f442eea54cd580249d2c6697ba4b2a
Drew Mueller <muellertandrew@gmail.com>
2020-09-30 14:46:56 -0400

    Added logo and form control of exiting

--- Lockdown/Form1.Designer.cs
+++ Lockdown/Form1.Designer.cs
@@ -29,20 +29,26 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.LogoPanel = new System.Windows.Forms.Panel();
             this.LogoImgBox = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
+            this.NavigationPanel = new System.Windows.Forms.Panel();
+            this.FormControlPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LogoPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LogoImgBox)).BeginInit();
+            this.NavigationPanel.SuspendLayout();
+            this.FormControlPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // LogoPanel
             // 
-            this.panel1.Controls.Add(this.LogoImgBox);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(244, 440);
-            this.panel1.TabIndex = 0;
+            this.LogoPanel.Controls.Add(this.LogoImgBox);
+            this.LogoPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LogoPanel.Location = new System.Drawing.Point(0, 0);
+            this.LogoPanel.Name = "LogoPanel";
+            this.LogoPanel.Size = new System.Drawing.Size(244, 440);
+            this.LogoPanel.TabIndex = 0;
             // 
             // LogoImgBox
             // 
@@ -55,26 +61,77 @@
             this.LogoImgBox.TabIndex = 0;
             this.LogoImgBox.TabStop = false;
             // 
+            // NavigationPanel
+            // 
+            this.NavigationPanel.Controls.Add(this.FormControlPanel);
+            this.NavigationPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NavigationPanel.Location = new System.Drawing.Point(244, 0);
+            this.NavigationPanel.Name = "NavigationPanel";
+            this.NavigationPanel.Size = new System.Drawing.Size(490, 440);
+            this.NavigationPanel.TabIndex = 1;
+            // 
+            // FormControlPanel
+            // 
+            this.FormControlPanel.Controls.Add(this.label2);
+            this.FormControlPanel.Controls.Add(this.label1);
+            this.FormControlPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.FormControlPanel.Location = new System.Drawing.Point(0, 0);
+            this.FormControlPanel.Name = "FormControlPanel";
+            this.FormControlPanel.Size = new System.Drawing.Size(490, 29);
+            this.FormControlPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(469, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(453, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "–";
+            // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(33)))), ((int)(((byte)(61)))));
             this.ClientSize = new System.Drawing.Size(734, 440);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.NavigationPanel);
+            this.Controls.Add(this.LogoPanel);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
+            this.LogoPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.LogoImgBox)).EndInit();
+            this.NavigationPanel.ResumeLayout(false);
+            this.FormControlPanel.ResumeLayout(false);
+            this.FormControlPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel LogoPanel;
         private System.Windows.Forms.PictureBox LogoImgBox;
+        private System.Windows.Forms.Panel NavigationPanel;
+        private System.Windows.Forms.Panel FormControlPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
